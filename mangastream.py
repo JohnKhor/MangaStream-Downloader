230c0c5b1df46b2d623212561411afb614488d87
JohnKhor <0jetpistol0@gmail.com>
2017-08-15 22:36:58 +0800

    Refactor

--- mangastream.py
+++ mangastream.py
@@ -1,144 +1,130 @@
-import bs4
-import os
-import prettytable
-import requests
+import bs4, os, prettytable, requests
 
-def select_manga():
-    # Get manga list page
-    manga_list_url = "http://mangastream.com/manga"
-    manga_list_res = requests.get(manga_list_url)
-    manga_list_res.raise_for_status()
-    
-    # Convert to soup for parsing
-    manga_list_soup = bs4.BeautifulSoup(manga_list_res.text, "html.parser")
+def to_html(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
 
-    # Get list of manga and print them
-    manga_list = manga_list_soup.select("td a")[::2]
-    manga_table = prettytable.PrettyTable(["Index", "Manga"])
-    for idx, manga in enumerate(manga_list):
-        manga_table.add_row([str(idx + 1), manga.getText()])
-    print(manga_table)
+def print_table(col_header, data):
+    table = prettytable.PrettyTable(["Index", col_header])
+    for idx, value in enumerate(data):
+        table.add_row([str(idx + 1), value])
+    print(table)
 
-    # User select manga
+def int_input(prompt, lower, upper):
     while True:
-        chosen_manga_idx = int(input("\nSelect the manga you want to download.\nPlease input the index on the left.\n")) - 1
-        if chosen_manga_idx >= 0 and chosen_manga_idx < len(manga_list):
+        user_input = int(input(prompt))
+        if user_input >= lower and user_input <= upper:
             break
         else:
-            print("Out of range. Index must be between 1 and " + str(len(manga_list)) + ".")
-    chosen_manga = manga_list[chosen_manga_idx]
-    print("\nYou have chosen " + chosen_manga.getText() + ".")
-    
-    # Create manga folder
-    os.makedirs(chosen_manga.getText(), exist_ok=True)
+            print("Out of range. Index must be between "+ str(lower) + "and " + str(upper) + ".")
+    return user_input - 1 # zero-based indexing
 
-    return chosen_manga
+def write_image(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
 
-def select_chapters(chosen_manga):
-    # Get chapter list page
-    chapter_list_url = chosen_manga.get("href")
-    chapter_list_res = requests.get(chapter_list_url)
-    chapter_list_res.raise_for_status()
-    
-    # Convert to soup for parsing
-    chapter_list_soup = bs4.BeautifulSoup(chapter_list_res.text, "html.parser")
+    img_file = open(filename, 'wb')
+    for chunk in response.iter_content(100000):
+        img_file.write(chunk)
+    img_file.close()
 
-    # Get list of chapters and print them
-    chapter_list = chapter_list_soup.select("td a")
-    chapter_table = prettytable.PrettyTable(["Index", "Chapter"])
-    for idx, chapter in enumerate(chapter_list):
-        chapter_table.add_row([str(idx + 1), chapter.getText()])
-    print(chapter_table)
+class Downloader:
+    def __init__(self):
+        # Soup objects
+        self.chosen_manga = None
+        self.chosen_chapters = None
 
-    # User select chapters
-    print("\nSelect the chapters you want to download.\nPlease input the index on the left.")
-    
-    # User select starting chapter
-    while True:
-        start_chapter = int(input("\nSelect the starting chapter.\n")) - 1
-        if start_chapter >= 0 and start_chapter < len(chapter_list):
-            break
-        else:
-            print("Out of range. Index must be between 1 and " + str(len(chapter_list)) + ".")
+    def select_manga(self):
+        # Parse manga list page
+        manga_list_url = "http://mangastream.com/manga"
+        manga_list_html = to_html(manga_list_url)
+        manga_list_soup = bs4.BeautifulSoup(manga_list_html, "html.parser")
 
-    # User select ending chapter
-    while True:
-        end_chapter = int(input("\nSelect the ending chapter.\n")) - 1
-        if end_chapter >= start_chapter and end_chapter < len(chapter_list):
-            break
-        else:
-            print("Out of range. Index must be between " + str(start_chapter + 1) + " and " + str(len(chapter_list)) + ".")
-    print()
+        # Get list of manga and print them
+        manga_list = manga_list_soup.select("td a")[::2]
+        print_table("Manga", [manga.getText() for manga in manga_list])
 
-    return chapter_list[start_chapter:end_chapter+1]
-
-def download_images(chosen_manga, chosen_chapters):
-    # Go through each chapter
-    for chapter in chosen_chapters:
-        print("Downloading " + chapter.getText() + " ", end="", flush=True)
-
-        # Create chapter folder
-        os.makedirs(os.path.join(chosen_manga.getText(), chapter.getText()), exist_ok=True)
+        # User select manga
+        chosen_manga_idx = int_input("\nSelect the manga you want to download.\nPlease input the index on the left.\n", 1, len(manga_list))
+        self.chosen_manga = manga_list[chosen_manga_idx]
+        print("\nYou have chosen " + self.chosen_manga.getText() + ".")
         
-        # Get chapter's first page
-        chapter_url = chapter.get("href")
-        page_res = requests.get(chapter_url)
-        page_res.raise_for_status()
-    
-        # Convert to soup for parsing
-        page_soup = bs4.BeautifulSoup(page_res.text, "html.parser")
+        # Create manga folder
+        os.makedirs(self.chosen_manga.getText(), exist_ok=True)
 
-        # Initialize page number
-        page_number = 1
+    def select_chapters(self):
+        # Parse chapter list page
+        chapter_list_url = self.chosen_manga.get("href")
+        chapter_list_html = to_html(chapter_list_url)
+        chapter_list_soup = bs4.BeautifulSoup(chapter_list_html, "html.parser")
 
-        # Go through each page of a chapter
-        while True:
-            print(".", end="", flush=True)
+        # Get list of chapters and print them
+        chapter_list = chapter_list_soup.select("td a")
+        print_table("Chapter", [chapter.getText() for chapter in chapter_list])
 
-            # Get source image page
-            img_url = "http:" + page_soup.select("#manga-page")[0].get("src")
-            img_res = requests.get(img_url)
-            img_res.raise_for_status()
+        # User select starting and ending chapters
+        print("\nSelect the chapters you want to download.\nPlease input the index on the left.")
+        start_chapter = int_input("\nSelect the starting chapter.\n", 1, len(chapter_list))
+        end_chapter = int_input("\nSelect the ending chapter.\n", start_chapter, len(chapter_list))
+        print()
 
-            # Stringify page number
-            if page_number < 10:
-                page_number_string = "00" + str(page_number)
-            elif page_number < 100:
-                page_number_string = "0" + str(page_number)
-            else:
-                page_number_string = str(page_number)
+        self.chosen_chapters = chapter_list[start_chapter:end_chapter+1]
 
-            # Get image file extension
-            img_extension = img_url.rsplit(".", 1)[1]
+    def download_images(self):
+        # Go through each chapter
+        for chapter in self.chosen_chapters:
+            print("Downloading " + chapter.getText() + " ", end="", flush=True)
 
-            # Download image
-            img_file = open(os.path.join(chosen_manga.getText(), chapter.getText(), page_number_string + "." + img_extension), 'wb')
-            for chunk in img_res.iter_content(100000):
-                img_file.write(chunk)
-            img_file.close()
+            # Create chapter folder
+            os.makedirs(os.path.join(self.chosen_manga.getText(), chapter.getText()), exist_ok=True)
+            
+            # Parse chapter's first page
+            page_url = chapter.get("href")
+            page_html = to_html(page_url)
+            page_soup = bs4.BeautifulSoup(page_html, "html.parser")
 
-            # Get next page url
-            next_page_url = page_soup.select(".next a")[0].get("href")
+            # Chapter url
+            chapter_url = page_url.rsplit('/', 1)[0]
 
-            # Are we at the last page? If so, go to the next chapter
-            if next_page_url[-3:] == "tip" or next_page_url.rsplit('/', 1)[0] != chapter_url.rsplit('/', 1)[0]:
-                print()
-                break
+            # Initialize page number
+            page_number = 1
 
-            # Get next page
-            page_res = requests.get(next_page_url)
-            page_res.raise_for_status()
+            # Go through each page of a chapter
+            while True:
+                print(".", end="", flush=True)
 
-            # Convert to soup for parsing
-            page_soup = bs4.BeautifulSoup(page_res.text, "html.parser")
+                # Source image url
+                img_url = "http:" + page_soup.select("#manga-page")[0].get("src")
+                
+                # Filename
+                page_number_string = str(page_number).zfill(3)
+                img_extension = img_url.rsplit(".", 1)[1]
+                filename = os.path.join(self.chosen_manga.getText(), chapter.getText(), page_number_string + "." + img_extension)
 
-            # Increment page number
-            page_number += 1
+                # Download image
+                write_image(img_url, filename)
+
+                # Get next page url
+                next_page_url = page_soup.select(".next a")[0].get("href")
+
+                # Are we at the last page? If so, go to the next chapter
+                if next_page_url[-3:] == "tip" or next_page_url.rsplit('/', 1)[0] != chapter_url:
+                    print()
+                    break
+
+                # Parse next page
+                page_html = to_html(next_page_url)
+                page_soup = bs4.BeautifulSoup(page_html, "html.parser")
+
+                # Increment page number
+                page_number += 1
 
 def main():
-    manga = select_manga()
-    chapters = select_chapters(manga)
-    download_images(manga, chapters)
+    downloader = Downloader()
+    downloader.select_manga()
+    downloader.select_chapters()
+    downloader.download_images()
 
 if __name__ == "__main__":
     main()
